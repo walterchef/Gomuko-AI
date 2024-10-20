184fd0250c13a10e6579bc8295575b52ab87c5db
walterchef <walter@sundling.nu>
2024-10-20 09:48:53 +0200

    Formatting

--- game.py
+++ game.py
@@ -5,22 +5,26 @@ from time import sleep
 
 
 class Game:
-    
-    def __init__(self, board: Board, graphics: Graphics, player1: Player, player2: Player):
+
+    def __init__(
+        self, board: Board, graphics: Graphics, player1: Player, player2: Player
+    ):
         self.board = board
         self.graphics = graphics
         self.player1 = player1
         self.player2 = player2
-        self.current_player = self.player1 if self.player1.symbol == "X" else self.player2
+        self.current_player = (
+            self.player1 if self.player1.symbol == "X" else self.player2
+        )
         self.winner = None
         self.running = True
-    
-        
+
     def switch_turns(self) -> None:
         """Byt vilken spelares tur det är."""
-        self.current_player = self.player2 if self.current_player == self.player1 else self.player1
-    
-    
+        self.current_player = (
+            self.player2 if self.current_player == self.player1 else self.player1
+        )
+
     def is_game_over(self) -> bool:
         """Kolla om en omgång är slut."""
         if self.board.is_terminal(self.player1.symbol, self.player2.symbol):
@@ -31,37 +35,39 @@ class Game:
             else:
                 self.winner = None
             return True
-        
-    
+
     def play_again(self):
         """Kolla om användaren vill spela igen."""
-        return self.graphics.wait_for_restart_or_quit()      
+        return self.graphics.wait_for_restart_or_quit()
 
-
-    def play(self) -> bool:  
+    def play(self) -> bool:
         """Spela en omgång tills att den är slut."""
         while self.running:
-            
+
             self.graphics.draw_board()
 
             # Hantera den nuvarande spelarens drag
             if isinstance(self.current_player, AI_Player):
                 move = self.current_player.make_move(self.board)
             elif isinstance(self.current_player, User_Player):
-                move = self.current_player.make_move(self.board, self.graphics.cell_size)
-            
+                move = self.current_player.make_move(
+                    self.board, self.graphics.cell_size
+                )
+
             # Markera draget på brädet
             self.board.mark_cell(self.current_player.symbol, move[0], move[1])
 
             # Kolla om en omgång är över
             if self.is_game_over():
                 self.graphics.draw_board()  # Redraw the final state of the board
-                self.graphics.display_game_over_message(self.winner)  # Display winner message
+                self.graphics.display_game_over_message(
+                    self.winner
+                )  # Display winner message
 
                 # Kolla om användaren vill spela igen
                 return self.play_again()
-            
+
             # Byt vems tur det är att göra ett drag om omgången inte är över
             self.switch_turns()
-        
-        return False  
+
+        return False
--- graphics.py
+++ graphics.py
@@ -1,8 +1,11 @@
 import pygame
 from board import *
 
+
 class Graphics:
-    def __init__(self, board, background_color=(30, 150, 140), line_color=(23, 140, 132)):
+    def __init__(
+        self, board, background_color=(30, 150, 140), line_color=(23, 140, 132)
+    ):
         self.board = board
         self.background_color = background_color
         self.line_color = line_color
@@ -16,7 +19,12 @@ class Graphics:
         max_board_pixel_size = 800
         min_size = 40
         max_size = 700
-        return min(max(max_board_pixel_size // max(self.board.rows, self.board.cols), min_size), max_size)
+        return min(
+            max(
+                max_board_pixel_size // max(self.board.rows, self.board.cols), min_size
+            ),
+            max_size,
+        )
 
     def calculate_line_width(self):
         min_width = 1
@@ -39,11 +47,21 @@ class Graphics:
     def display_grid_lines(self):
         """Rita upp rutnätets linjer"""
         for col in range(1, self.board.cols):
-            pygame.draw.line(self.screen, self.line_color, (col * self.cell_size, 0), 
-                             (col * self.cell_size, self.height), self.line_width)
+            pygame.draw.line(
+                self.screen,
+                self.line_color,
+                (col * self.cell_size, 0),
+                (col * self.cell_size, self.height),
+                self.line_width,
+            )
         for row in range(1, self.board.rows):
-            pygame.draw.line(self.screen, self.line_color, (0, row * self.cell_size), 
-                             (self.width, row * self.cell_size), self.line_width)
+            pygame.draw.line(
+                self.screen,
+                self.line_color,
+                (0, row * self.cell_size),
+                (self.width, row * self.cell_size),
+                self.line_width,
+            )
 
     def display_pieces(self):
         """Rita upp figurerna X och O."""
@@ -58,17 +76,35 @@ class Graphics:
         """Rita ett X i en given cell."""
         offset = self.cell_size // 5
         start_desc = (col * self.cell_size + offset, row * self.cell_size + offset)
-        end_desc = (col * self.cell_size + self.cell_size - offset, row * self.cell_size + self.cell_size - offset)
-        pygame.draw.line(self.screen, (66, 66, 66), start_desc, end_desc, self.line_width * 2)
-        start_asc = (col * self.cell_size + offset, row * self.cell_size + self.cell_size - offset)
-        end_asc = (col * self.cell_size + self.cell_size - offset, row * self.cell_size + offset)
-        pygame.draw.line(self.screen, (66, 66, 66), start_asc, end_asc, self.line_width * 2)
+        end_desc = (
+            col * self.cell_size + self.cell_size - offset,
+            row * self.cell_size + self.cell_size - offset,
+        )
+        pygame.draw.line(
+            self.screen, (66, 66, 66), start_desc, end_desc, self.line_width * 2
+        )
+        start_asc = (
+            col * self.cell_size + offset,
+            row * self.cell_size + self.cell_size - offset,
+        )
+        end_asc = (
+            col * self.cell_size + self.cell_size - offset,
+            row * self.cell_size + offset,
+        )
+        pygame.draw.line(
+            self.screen, (66, 66, 66), start_asc, end_asc, self.line_width * 2
+        )
 
     def draw_circle(self, row, col):
         """Rita ett O i en given cell."""
-        center = (col * self.cell_size + self.cell_size // 2, row * self.cell_size + self.cell_size // 2)
+        center = (
+            col * self.cell_size + self.cell_size // 2,
+            row * self.cell_size + self.cell_size // 2,
+        )
         radius = self.cell_size // 3
-        pygame.draw.circle(self.screen, (66, 66, 66), center, radius, self.line_width * 2)
+        pygame.draw.circle(
+            self.screen, (66, 66, 66), center, radius, self.line_width * 2
+        )
 
     def draw_button(self, text, rect, color):
         """Rita upp en knapp"""
@@ -83,13 +119,16 @@ class Graphics:
         x_button_color = (0, 128, 0)
         o_button_color = (128, 0, 0)
         button_width, button_height = 100, 50
-        x_button_rect = pygame.Rect(self.width // 2 - 150, self.height // 2, button_width, button_height)
-        o_button_rect = pygame.Rect(self.width // 2 + 50, self.height // 2, button_width, button_height)
+        x_button_rect = pygame.Rect(
+            self.width // 2 - 150, self.height // 2, button_width, button_height
+        )
+        o_button_rect = pygame.Rect(
+            self.width // 2 + 50, self.height // 2, button_width, button_height
+        )
 
         font = pygame.font.Font(None, 60)
         self.draw_board()
 
-        # Render welcome message and buttons
         welcome_text = font.render("Welcome to Five in a Row", True, (255, 255, 255))
         welcome_rect = welcome_text.get_rect(center=(self.width // 2, self.height // 4))
         self.screen.blit(welcome_text, welcome_rect)
@@ -108,7 +147,6 @@ class Graphics:
                         return ["X", "O"]
                     if o_button_rect.collidepoint(mouse_pos):
                         return ["O", "X"]
-                    
 
     def display_game_over_message(self, winner=None):
         """Skriv ut meddelande när en spelomgång är över."""
@@ -118,8 +156,6 @@ class Graphics:
         text_rect = text.get_rect(center=(self.width // 2, self.height // 2))
         self.screen.blit(text, text_rect)
         pygame.display.update()
-        
-        
 
     def wait_for_restart_or_quit(self):
         """Ge användaren möjligheten att spela igen eller avsluta."""
@@ -127,12 +163,18 @@ class Graphics:
         play_again_text = font.render("PLAY AGAIN", True, (0, 0, 0))
         quit_text = font.render("QUIT GAME", True, (0, 0, 0))
 
-        play_again_button = pygame.Rect(self.width // 2 - 100, self.height // 2 + 50, 200, 40)
-        quit_button = pygame.Rect(self.width // 2 - 100, self.height // 2 + 100, 200, 40)
+        play_again_button = pygame.Rect(
+            self.width // 2 - 100, self.height // 2 + 50, 200, 40
+        )
+        quit_button = pygame.Rect(
+            self.width // 2 - 100, self.height // 2 + 100, 200, 40
+        )
 
         pygame.draw.rect(self.screen, (0, 255, 0), play_again_button)
         pygame.draw.rect(self.screen, (255, 0, 0), quit_button)
-        self.screen.blit(play_again_text, (play_again_button.x + 50, play_again_button.y + 5))
+        self.screen.blit(
+            play_again_text, (play_again_button.x + 50, play_again_button.y + 5)
+        )
         self.screen.blit(quit_text, (quit_button.x + 50, quit_button.y + 5))
         pygame.display.update()
 
